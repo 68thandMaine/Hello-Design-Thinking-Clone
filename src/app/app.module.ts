bbd5aabbf91279f7f175f8ddcdae24ba7da29b9e
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-01-25 09:59:39 -0800

    Add a compnent for the log in, the registration, and the navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BackgroundComponent } from './background/background.component';
+import { RegisterComponent } from './register/register.component';
+import { LoginComponent } from './login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    BackgroundComponent
+    BackgroundComponent,
+    RegisterComponent,
+    LoginComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
