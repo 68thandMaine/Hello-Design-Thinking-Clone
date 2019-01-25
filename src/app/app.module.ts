52858471262ae56f9939e5980e31dbbc7a62cdb9
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-01-25 09:58:21 -0800

    Add a backround component to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { BackgroundComponent } from './background/background.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundComponent
   ],
   imports: [
     BrowserModule
