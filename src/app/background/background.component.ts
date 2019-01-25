489cd15f29ecfb3845841a91d484944aa5c10b37
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-01-25 10:55:44 -0800

    Add cycling background image script to backgorund.component.ts

--- src/app/background/background.component.ts
+++ src/app/background/background.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-background',
   templateUrl: './background.component.html',
   styleUrls: ['./background.component.css']
 })
-export class BackgroundComponent implements OnInit {
+export class BackgroundComponent  {
 
-  constructor() { }
 
-  ngOnInit() {
+  let index = 0;
+  function changeBanner() {
+      [].forEach.call(document.images, function(v, i) {
+          document.images[i].hidden = i !== index
+      });
+      index = (index + 1) % document.images.length;
   }
+  window.onload = function() {
+      setInterval(changeBanner, 4000)
+  };
 
 }
