2075f8594c77da826ffc0c7334a282ef9bc58668
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-01-25 12:38:58 -0800

    Add styling to navbar logos and add text for links

--- src/app/background/background.component.ts
+++ src/app/background/background.component.ts
@@ -6,17 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./background.component.css']
 })
 export class BackgroundComponent  {
-
-
-  let index = 0;
-  function changeBanner() {
-      [].forEach.call(document.images, function(v, i) {
-          document.images[i].hidden = i !== index
-      });
-      index = (index + 1) % document.images.length;
-  }
-  window.onload = function() {
-      setInterval(changeBanner, 4000)
-  };
-
-}
+  constructor() { }
