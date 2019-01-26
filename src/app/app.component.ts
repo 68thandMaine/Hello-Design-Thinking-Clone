7e1dbc77587cc253cb684b0ab97ddfe394816d0b
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-01-25 17:06:15 -0800

    Trying to update changes from branches being wonky

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './models/user.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Hello Design Thinking Clone';
+
+  masterUserList: User[] = [ ];
 }
--- src/app/user/user.component.spec.ts
+++ src/app/user/user.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { RegisterComponent } from './register.component';
+import { userComponent } from './user.component';
 
-describe('RegisterComponent', () => {
-  let component: RegisterComponent;
-  let fixture: ComponentFixture<RegisterComponent>;
+describe('userComponent', () => {
+  let component: userComponent;
+  let fixture: ComponentFixture<userComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
+      declarations: [ userComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(RegisterComponent);
+    fixture = TestBed.createComponent(userComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
