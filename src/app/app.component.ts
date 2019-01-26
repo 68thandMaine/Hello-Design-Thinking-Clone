0625f509768a030ff5c4e4d7a01c10d1529cad2c
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-01-25 17:44:24 -0800

    I think I've added a basic way to create a new user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { User } from './models/user.model';
 
 @Component({
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { User } from '../models/user/model';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class userComponent implements OnInit {
+export class userComponent  {
+  @Input() userList: User [];
+  @Output() clickSender = new EventEmitter();
 
   constructor() { }
 
-  ngOnInit() {
-  }
-
+submitRegistration() {
+  let first_Name = $("#firstName").val();
+  let last_Name = $("#lastName").val();
+  let company_Name = $("#companyName").val();
+  let job_Title = $("#jobTitle").val();
+  let email = $("#email").val();
+  let password = $("#password").val();
+  
+}
 }
