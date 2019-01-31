d9cc7319e3228fbf6ed0cf8e556385b17c89887b
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-01-31 13:14:38 -0800

    Wrote code for importing routing, filled out decorators and added properties and items to the constructor

--- src/app/edit-user/edit.component.ts
+++ src/app/edit-user/edit.component.ts
@@ -1,12 +1,32 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common'
+import { UserService } from '../user.service';
+import { FirebaseObjectObservable } from 'angularfire2/database';
 import { User } from '../models/user.model';
 
 @Component({
   selector: 'app-edit-user',
   templateUrl: './edit.component.html',
-  styleUrls: ['./edit.component.css']
+  styleUrls: ['./edit.component.css'],
+  providers: [UserService]
 })
-export class EditComponent  {
 
+export class EditComponent implements OnInit {
+  userId: string;
+  userToDisplay;
+
+  constructor(
+    private route: ActivatedRoute,
+    private location: Location,
+    private userService: UserService
+  ) { }
+
+  ngOnInit() {
+    this.route.params.forEach((urlParameters) => {
+      this.userId = urlParameters['id'];
+    });
+    this.userToDisplay = this.userService.getUserById(this.userId);
+  }
 
 }
