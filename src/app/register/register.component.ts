820e8bf086fb60965b73cac56ed7e008b60c8f8b
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-01-31 16:48:05 -0800

    Add authorization to register componnet to signup a user with email and password:

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { UserService } from '../user.service';
 import { User } from '../models/user.model';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
+
+
+
+
 
 @Component({
   selector: 'app-register',
@@ -11,6 +17,7 @@ export class RegisterComponent {
 constructor(private userService: UserService) { }
 
   submitRegistration(firstName: string, lastName: string, companyName: string, jobTitle: string, email: string, password: string, confirmPassword: string) {
+    firebase.auth().createUserWithEmailAndPassword(email,password)
     let newUser: User = new User(firstName, lastName, companyName, jobTitle, email, password, confirmPassword);
     this.userService.addNewUser(newUser)
   }
