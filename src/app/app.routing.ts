521f75f453cf547a72453c286ce5c4585d79c17b
Chris Rudnicky <chrisrudnicky@gmail.com>
2019-02-01 11:12:32 -0800

    Add routing file

---
+++ src/app/app.routing.ts
@@ -0,0 +1,13 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const appRoutes: Routes = [
+  {
+    path: '',
+    component:
+  },
+
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
