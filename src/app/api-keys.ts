d449d1e73c41e988071a8e2164909d2b298ee9af
Chris Rudnicky <43099543+68thandMaine@users.noreply.github.com>
2019-02-01 08:26:22 -0800

    Delete api-keys.ts
    Deleting API key data

--- src/app/api-keys.ts
+++
@@ -1,8 +0,0 @@
-export const masterFirebaseConfig = {
-  apiKey: "AIzaSyCrSdEoljevK9vsApen-P9GaseS7iE7YEE",
-  authDomain: "hdt-clone.firebaseapp.com",
-  databaseURL: "https://hdt-clone.firebaseio.com",
-  projectId: "hdt-clone",
-  storageBucket: "hdt-clone.appspot.com",
-  messagingSenderId: "1055349485796"
-}
